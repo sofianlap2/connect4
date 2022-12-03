c5d6358baf1a8c66a124a1f99456852167c5c106
sofiane nabli <sofienne.nabli@aleia.io>
2022-12-03 18:10:49 +0100

    connect4

---
+++ connect4.js
@@ -0,0 +1,116 @@
+var playerRed = "R";
+var playerYellow = "Y";
+var currPlayer = playerRed;
+
+var gameOver = false;
+var board;
+
+var rows = 6;
+var columns = 7;
+var currColumns = []; //keeps track of which row each column is at.
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = []
+    currColumns = [5, 5, 5, 5, 5, 5, 5]
+    for(let r = 0; r < rows; r++) {
+        let row = []
+        for(let c = 0; c < columns; c++) {
+            // JS
+            row.push(' ');
+            // HTML
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
+        }
+        board.push(row)
+    }
+}
+
+function setPiece() {
+    if(gameOver) {
+        return
+    }
+    let coords = this.id.split('-')
+    let r = parseInt(coords[0])
+    let c = parseInt(coords[1])
+
+    r = currColumns[c]
+    if(r < 0) {
+        return
+    }
+    board[r][c] = currPlayer
+    let tile = document.getElementById(r.toString() + '-' + c.toString())
+
+    if(currPlayer === playerRed) {
+        tile.classList.add('red-piece')
+        currPlayer = playerYellow
+    } else {
+        tile.classList.add('yellow-piece')
+        currPlayer = playerRed
+    }
+
+    r--
+    currColumns[c] = r
+
+    checkWinner()
+}
+
+function checkWinner() {
+    for(let r = 0; r < rows; r++) {
+        for(let c = 0; c < columns - 3; c++) {
+            if(board[r][c] !== ' ') {
+                if(board[r][c] === board[r][c + 1] && board[r][c + 1] === board[r][c + 2] && board[r][c + 2] === board[r][c + 3]) {
+                    setWinner(r, c)
+                    return
+                }
+            }
+        }
+    }
+
+    for(let c = 0; c < columns; c++) {
+        for(let r = 0; r < rows - 3; r++) {
+            if(board[r][c] !== ' ') {
+                if(board[r][c] === board[r + 1][c] && board[r + 1][c] === board[r + 2][c] && board[r + 2][c] === board[r + 3][c]) {
+                    setWinner(r, c)
+                    return
+                }
+            }
+        }
+    }
+
+    for(let r = 0; r < rows - 3; r++) {
+        for(let c = 0; c < columns - 3; c++) {
+            if(board[r][c] !== ' ') {
+                if(board[r][c] === board[r + 1][c + 1] && board[r + 1][c + 1] === board[r + 2][c + 2] && board[r + 2][c + 2] === board[r + 3][c + 3]) {
+                    setWinner(r, c)
+                    return
+                }
+            }
+        }
+    }
+
+    for(let r = 3; r < rows; r++) {
+        for(let c = 0; c < columns - 3; c++) {
+            if(board[r][c] !== ' ') {
+                if(board[r][c] === board[r - 1][c + 1] && board[r - 1][c + 1] === board[r - 2][c + 2] && board[r - 2][c + 2] === board[r - 3][c + 3]) {
+                    setWinner(r, c)
+                    return
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r, c) {
+    if(board[r][c] === playerRed) {
+        document.getElementById('winner').innerText = 'player red has won'
+    } else {
+        document.getElementById('winner').innerText = 'player yellow has won'
+    }
+}
